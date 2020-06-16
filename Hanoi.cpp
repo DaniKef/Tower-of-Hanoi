22dd670a785b521b8d215d828b2a941dd2da44f2
DaniKef <66998903+DaniKef@users.noreply.github.com>
2020-06-16 21:05:17 +0300

    Create Hanoi.cpp

---
+++ Hanoi.cpp
@@ -0,0 +1,11 @@
+void Tower(int n, char from_rod,char to_rod, char aux_rod)
+{
+	if (n == 1)
+	{
+		cout << "Move disk 1 from rod " << from_rod <<" to rod " << to_rod << endl;
+		return;
+	}
+	Tower(n - 1, from_rod, aux_rod, to_rod);
+	cout << "Move disk " << n << " from rod " << from_rod <<" to rod " << to_rod << endl;
+	Tower(n - 1, aux_rod, to_rod, from_rod);
+}
